6de39b76f8be13e8f8d7757d65d456b6fabc02e2
KOREAIT <KOREAIT@DESKTOP-7URGO01>
2021-12-10 10:01:57 +0900

    비밀번호 mail임시변경 완료

--- WithDog/src/main/java/com/withdog/service/memberServiceImpl.java
+++ WithDog/src/main/java/com/withdog/service/memberServiceImpl.java
@@ -63,28 +63,23 @@ public class memberServiceImpl implements MemberService {
 	}
 	
 	public void sendHtmlEmail(String text,String id) {
-	    HtmlEmail email = new HtmlEmail();
-	    SimpleEmail email2 = new SimpleEmail();
-	    try {
-			email2.setHostName("smtp.naver.com");
-			email2.setSmtpPort(107);
-			email2.setSSL(true);
-			try {
-				email2.addTo(id);
-			} catch (EmailException e) {
-				e.printStackTrace();
-			}
-			try {
-				email2.setFrom("WithDogAdmin@naver.com","admin");
-			} catch (EmailException e) {
-				e.printStackTrace();
-			}
-			email2.setSubject("WithDog의 새비밀번호입니다.");
-			email2.setContent("<!DOCTYPE html>\r\n"
+		 HtmlEmail email = new HtmlEmail();
+		    try {
+		    email.setCharset("euc-kr");
+		    email.setHostName("smtp.gmail.com");
+		    email.setSmtpPort(465);
+		    email.addTo(id, "");
+		    email.setFrom("WithDog@admin.com", "");
+		    email.setAuthentication("본인gmail아이디", "본인gmail비밀번호");
+		    email.setTLS(true);
+		    email.setSSL(true);
+		    email.setSubject("WithDog의 새비밀번호입니다.");
+		    
+		    email.setHtmlMsg("<!DOCTYPE html>\r\n"
 		    		+ "<html lang=\"en\">\r\n"
 		    		+ "<head>\r\n"
 		    		+ "    <meta charset=\"UTF-8\">\r\n"
-		    		+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"I	E=edge\">\r\n"
+		    		+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
 		    		+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
 		    		+ "    <title>Document</title>\r\n"
 		    		+ "</head>\r\n"
@@ -93,14 +88,14 @@ public class memberServiceImpl implements MemberService {
 		    		+ "        <tbody><tr><td>\r\n"
 		    		+ "              <div style=\"width: 600px;\">\r\n"
 		    		+ "                <h1 style=\"margin-top: 30px; margin-bottom: 16px;\">\r\n"
-		    		+ "                  <a href=\"https://www.catpre.com\" target=\"_blank\" rel=\"noreferrer noopener\"><img style=\"border: 0px currentColor; border-image: none; display: block;\" alt=\"고양이대통령\" src=\"http://static.catpre.com/images/catpre/email/logo.png\" loading=\"lazy\"></a>\r\n"
+		    		+ "                  <a href=\"http://localhost:9081/WithDog/\" target=\"_blank\" rel=\"noreferrer noopener\"><img style=\"border: 0px currentColor; border-image: none; display: block;\" alt=\"WithDog\" src=\"http://appdata.hungryapp.co.kr/images/hatdog/img/pc_img/common/logo.png\" loading=\"lazy\"></a>\r\n"
 		    		+ "                </h1>\r\n"
 		    		+ "                <table width=\"600\" cellspacing=\"0\" cellpadding=\"0\" style=\"width:100%; border-radius:3px; border:1px solid rgb(221, 221, 221); border-image: none; -moz-border-radius:3px; -webkit-border-radius:3px;\">\r\n"
 		    		+ "                  <tbody><tr><td style=\"padding:30px;\">\r\n"
 		    		+ "                        <table style=\"width:100%; border-collapse: collapse; border-spacing: 0;\" cellspacing=\"0\" cellpadding=\"0\">\r\n"
 		    		+ "                          <tbody><tr><td align=\"left\" valign=\"top\" style=\"padding: 0px;\">\r\n"
 		    		+ "                                <!-- 회색 박스 안쪽 내용 -->\r\n"
-		    		+ "                                <h2 style=\"margin: 0px; color: rgba(162, 0, 199, 1); font-family: 돋움,dotum,sans-serif; font-size: 20px;\">\r\n"
+		    		+ "                                <h2 style=\"margin: 0px; color: rgba(255, 128, 13,100); font-family: 돋움,dotum,sans-serif; font-size: 20px;\">\r\n"
 		    		+ "                                  비밀번호 재설정 안내\r\n"
 		    		+ "                                </h2>\r\n"
 		    		+ "                                <div style=\"margin: 0px; padding: 0px; height: 24px;\">\r\n"
@@ -112,7 +107,7 @@ public class memberServiceImpl implements MemberService {
 		    		+ "                                    메일입니다.<br>아래의 비밀번호로 로그인 하신 뒤 마이페이지에서 비밀번호 변경 후 사용하세요.\r\n"
 		    		+ "                                  </p>\r\n"
 		    		+ "                                  <div style=\"margin: 0px; padding: 34px 0px 0px; text-align: center;\">\r\n"
-		    		+ "                                    <a style=\"background: rgba(162, 0, 199, 1); padding: 0px; border-radius: 4px; border: 0; border-image: none; text-align: center; color: #fff; font-size: 15px; font-weight: bold; text-decoration: none; display: inline-block;\"  target=\"_blank\" rel=\"noreferrer noopener\"><span style=\"padding: 0px 30px; height: 38px; line-height: 40px;\">"+text+"</span></a>\r\n"
+		    		+ "                                    <a style=\"background: rgba(255, 128, 13,100); padding: 0px; border-radius: 4px; border: 0; border-image: none; text-align: center; color: #fff; font-size: 15px; font-weight: bold; text-decoration: none; display: inline-block;\"  target=\"_blank\" rel=\"noreferrer noopener\"><span style=\"padding: 0px 30px; height: 38px; line-height: 40px;\">"+text+"</span></a>\r\n"
 		    		+ "                                  </div>\r\n"
 		    		+ "                                  <div style=\"margin: 0px; padding: 0px; height: 34px;\">\r\n"
 		    		+ "                                    &nbsp;&nbsp;&nbsp;\r\n"
@@ -137,8 +132,8 @@ public class memberServiceImpl implements MemberService {
 		    		+ "      \r\n"
 		    		+ "                <div style=\"width:100%; height:47px; overflow:hidden; border-radius:3px; -webkit-border-radius:3px;\">\r\n"
 		    		+ "                  <table width=\"600\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\r\n"
-		    		+ "                    <tbody><tr><td height=\"47\" style=\"color:#fff; font-size:14px; font-weight:bold; text-align:center; background-color:rgba(95,95,95, 1);\">\r\n"
-		    		+ "                          고양이대통령 고객센터 0000-0000\r\n"
+		    		+ "                    <tbody><tr><td height=\"47\" style=\"color:#fff; font-size:14px; font-weight:bold; text-align:center; background-color:rgba(255,128,13, 100);\">\r\n"
+		    		+ "                          WithDog 고객센터 0000-0000\r\n"
 		    		+ "                        </td></tr></tbody>\r\n"
 		    		+ "                  </table>\r\n"
 		    		+ "                </div>\r\n"
@@ -171,11 +166,11 @@ public class memberServiceImpl implements MemberService {
 		    		+ "            </td></tr></tbody>\r\n"
 		    		+ "      </table>\r\n"
 		    		+ "</body>\r\n"
-		    		+ "</html>","text/plain; charset=euc-kr");
-			email2.setAuthentication("gkswldnwldn@naver.com", "Gkswldn1234!");
-			email2.send();
-		} catch (EmailException e) {
-			e.printStackTrace();
+		    		+ "</html>");
+		    email.send();
+		    }catch(Exception e) {
+		    	e.printStackTrace();
+		    }
 		}
 	    
 //	    
@@ -285,7 +280,6 @@ public class memberServiceImpl implements MemberService {
 //	    }catch(Exception e) {
 //	    	e.printStackTrace();
 //	    }
-	}
 	
 	public String randomPwCreate(int size) {
 		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }; 
