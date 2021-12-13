13781e5d47914b37b375ae1ad758f12d0eb828f6
Administrator <Administrator@DESKTOP-CH2MQ98>
2021-12-14 01:03:01 +0900

    booking,hotel mapper 수정 / hotelList 페이징, 검색, 정렬 ajax화 진행중 (YS)

--- WithDog/src/main/java/com/withdog/controller/HotelController.java
+++ WithDog/src/main/java/com/withdog/controller/HotelController.java
@@ -1,11 +1,16 @@
 package com.withdog.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.withdog.dto.CriteriaDto;
@@ -37,23 +42,33 @@ public class HotelController {
 	}
 	
 	@RequestMapping(value="/hotelListM", method=RequestMethod.GET)
-	public String hotelListM(@RequestParam(value="p", required=false, defaultValue="1") int page,
-			@RequestParam(value="f", required=false, defaultValue="") String field,
-			@RequestParam(value="c", required=false, defaultValue="h_name") String category,
-			@RequestParam(value="o", required=false, defaultValue="recent") String order,
-			Model model) {
-		CriteriaDto cDto = new CriteriaDto(page,6,hotelService.getHotelCount(field, category), field, category, order);
+	public String hotelListM(Model model) {
+		CriteriaDto cDto = new CriteriaDto(1,6,hotelService.getHotelCount("", "h_name"), "", "h_name", "recent");
 		model.addAttribute("h_list",hotelService.getHotelList(cDto));
 		model.addAttribute("h_crit",cDto);
 		return "hotel/managerHotelList";	
 	}
 	
+//	
+//	@RequestMapping(value="/hotelListM", method=RequestMethod.GET)
+//	public String hotelListM(@RequestParam(value="p", required=false, defaultValue="1") int page,
+//			@RequestParam(value="f", required=false, defaultValue="") String field,
+//			@RequestParam(value="c", required=false, defaultValue="h_name") String category,
+//			@RequestParam(value="o", required=false, defaultValue="recent") String order,
+//			Model model) {
+//		CriteriaDto cDto = new CriteriaDto(page,6,hotelService.getHotelCount(field, category), field, category, order);
+//		model.addAttribute("h_list",hotelService.getHotelList(cDto));
+//		model.addAttribute("h_crit",cDto);
+//		return "hotel/managerHotelList";	
+//	}
+//	
 	@RequestMapping(value="/hotelListM", method=RequestMethod.POST)
-	public String hotelListM(String field, String category, Model model) {
-		CriteriaDto cDto = new CriteriaDto(1,6,hotelService.getHotelCount(field, category), field, category, "recent");
-		model.addAttribute("h_list",hotelService.getHotelList(cDto));
-		model.addAttribute("h_crit",cDto);
-		return "hotel/managerHotelList";
+	public @ResponseBody Map<String, Object> hotelListM(@RequestBody CriteriaDto datas) {
+		CriteriaDto cDto = new CriteriaDto(datas.getPage(),6,hotelService.getHotelCount(datas.getField(),datas.getCategory()), datas.getField(),datas.getCategory(), datas.getOrder());
+		Map<String, Object> maps = new HashMap<String, Object>();
+		maps.put("h_list",hotelService.getHotelList(cDto));
+		maps.put("h_crit", cDto);
+		return maps;	
 	}
 	
 	@RequestMapping(value="/updateHotelM", method=RequestMethod.GET)
